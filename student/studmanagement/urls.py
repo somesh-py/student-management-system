e3b24ffe4c0f6121f89176fdf334b5d75a0913cf
somesh-py <someshsharma615@gmail.com>
2023-02-17 20:51:02 +0530

    done connectvity with each other file

--- student/studmanagement/urls.py
+++ student/studmanagement/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index),
+    path('sign_in/',views.sign_in),
     path('courses/',views.courses),
     path('dashboard/',views.dashboard),
     path('employees/',views.employees),
@@ -9,6 +10,7 @@ urlpatterns = [
     path('pg_dashboard/',views.pg_dashboard),
     path('profile/',views.profile),
     path('sign-up/',views.sign_up),
+    path('signup_data/',views.signup_updata),
     path('tables/',views.tables),
     path('tenants/',views.tenants),
     path('viewstudents/',views.viewstudents),
--- student/studmanagement/views.py
+++ student/studmanagement/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.hashers import make_password,check_password
+from .models import Student
+from django.http.response import HttpResponse
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+def sign_in(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        ppassword=request.POST['password']
+        if Student.objects.filter(email=email).exists():
+            obj=Student.objects.get(email=email)
+            mpassword=obj.password
+            if check_password(ppassword,mpassword):
+                return redirect('/courses/')
+            else:
+                return render(request,'index.html',{'msg':'password was incorrect'})
+        else:
+            return render(request,'index.html',{'msg':'email not exist'})
 
 
 def courses(request):
@@ -33,10 +49,20 @@ def profile(request):
 
 def sign_up(request):
     return render(request, 'sign-up.html')
-
-
+def signup_updata(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        password= make_password(request.POST['password'])
+        if Student.objects.filter(email=email).exists():
+            messages.error(request,'email already exist')
+            return redirect('/sign-up/')
+        else:
+            Student.objects.create(name=name,email=email,password=password)
+            messages.success(request,'registration done sucessfully')
+            return redirect('/sign-up/')
 def tables(request):
-    return render(request, 'tables.html')
+    return render(request,'tables.html')
 
 
 def tenants(request):
