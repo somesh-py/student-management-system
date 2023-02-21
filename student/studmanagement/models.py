15ec4d84739a10ca7cf98cc1ce3212e6ec19bcee
somesh-py <someshsharma615@gmail.com>
2023-02-21 10:21:51 +0530

    perform add delete course operations

--- student/studmanagement/models.py
+++ student/studmanagement/models.py
@@ -6,3 +6,9 @@ class Student(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=100)
     password=models.CharField(max_length=100)
+
+class Addcourse(models.Model):
+    course=models.CharField(max_length=100)
+    fees=models.CharField(max_length=20000000)
+    date=models.DateTimeField(auto_now_add=True)
+    comment=models.CharField(max_length=100000000)
--- student/studmanagement/views.py
+++ student/studmanagement/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.hashers import make_password,check_password
-from .models import Student
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from .models import *
 from django.http.response import HttpResponse
 from django.contrib import messages
 # Create your views here.
@@ -8,23 +8,26 @@ from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
+
+
 def sign_in(request):
-    if request.method=='POST':
-        email=request.POST['email']
-        ppassword=request.POST['password']
+    if request.method == 'POST':
+        email = request.POST['email']
+        ppassword = request.POST['password']
         if Student.objects.filter(email=email).exists():
-            obj=Student.objects.get(email=email)
-            mpassword=obj.password
-            if check_password(ppassword,mpassword):
+            obj = Student.objects.get(email=email)
+            mpassword = obj.password
+            if check_password(ppassword, mpassword):
                 return redirect('/courses/')
             else:
-                return render(request,'index.html',{'msg':'password was incorrect'})
+                return render(request, 'index.html', {'msg': 'password was incorrect'})
         else:
-            return render(request,'index.html',{'msg':'email not exist'})
+            return render(request, 'index.html', {'msg': 'email not exist'})
 
 
 def courses(request):
-    return render(request, 'courses.html')
+    data = Addcourse.objects.all()
+    return render(request, 'courses.html', {'data': data})
 
 
 def dashboard(request):
@@ -49,20 +52,24 @@ def profile(request):
 
 def sign_up(request):
     return render(request, 'sign-up.html')
+
+
 def signup_updata(request):
-    if request.method=='POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        password= make_password(request.POST['password'])
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = make_password(request.POST['password'])
         if Student.objects.filter(email=email).exists():
-            messages.error(request,'email already exist')
+            messages.error(request, 'email already exist')
             return redirect('/sign-up/')
         else:
-            Student.objects.create(name=name,email=email,password=password)
-            messages.success(request,'registration done sucessfully')
+            Student.objects.create(name=name, email=email, password=password)
+            messages.success(request, 'registration done sucessfully')
             return redirect('/sign-up/')
+
+
 def tables(request):
-    return render(request,'tables.html')
+    return render(request, 'tables.html')
 
 
 def tenants(request):
@@ -71,3 +78,37 @@ def tenants(request):
 
 def viewstudents(request):
     return render(request, 'viewstudents.html')
+
+
+def addcourse(request):
+    if request.method == 'POST':
+        course = request.POST['course']
+        fees = request.POST['fees']
+        date = request.POST['date']
+        comment = request.POST['comment']
+        Addcourse.objects.create(
+            course=course, fees=fees, date=date, comment=comment)
+        data = Addcourse.objects.all()
+        return render(request, 'courses.html', {'data': data})
+
+
+def updatepage(request, uid):
+    res = Addcourse.objects.get(id=uid)
+    return render(request, 'updateform.html', {'i': res})
+
+
+def course_update(request):
+    if request.method == 'POST':
+        uid = request.POST['uid']
+        ccourse = request.POST['ucourse']
+        cfees = request.POST['ufees']
+        ccomment = request.POST['ucomment']
+        cdate = request.POST['uregistration']
+        Addcourse.objects.filter(id=uid).update(
+            course=ccourse, fees=cfees, comment=ccomment, date=cdate)
+        return redirect('/courses/')
+
+
+def delete(request,pk):
+    res = Addcourse.objects.filter(id=pk).delete()
+    return redirect('/courses/')
